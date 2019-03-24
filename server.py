254997bb311e702a530fef27cf8014eb595bb912
Vitaly Sizov <v.sizov@bk.ru>
2019-03-24 14:24:56 +0300

    Add files
    Add all new files

---
+++ server.py
@@ -0,0 +1,17 @@
+from bottle import route, run
+from datetime import datetime as dt
+import os
+
+
+@route("/")
+def index():
+  now = dt.now()
+  return {
+    "Проверка Heroku": "OK",
+    "date": f"{now.day} {now.month} {now.year}"
+  }
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
