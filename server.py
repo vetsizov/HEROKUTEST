60ea65f711d1f2ffb14e29a5e87e8bf0b2d4b5c3
Vitaly Sizov <v.sizov@bk.ru>
2019-04-14 12:12:46 +0300

    Update server.py

--- server.py
+++ server.py
@@ -8,10 +8,14 @@ def index():
   now = dt.now()
   return {
     "Проверка Heroku": "OK3",
-    "date": f"{now.day} {now.month} {now.year}"
+    "date": f"{now.day} {now.month} {now.year}",
+    "Секретное значение": s,
   }
 
 if os.environ.get('APP_LOCATION') == 'heroku':
+    s = os.environ.get("secret_x")
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 else:
+    s = 888
     run(host='localhost', port=8080, debug=True)
